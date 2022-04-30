c3f73f557ca985d53a7d28a0e68ec2b7dd27dfa6
gautamgc17 <gautamabd17@gmail.com>
2022-04-30 16:08:11 +0530

    updated backend

--- checkup/settings.py
+++ checkup/settings.py
@@ -124,29 +124,19 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR , 'static'), )
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'feedbackmonitor123@gmail.com'
-EMAIL_HOST_PASSWORD = 'shakalakaboom!@12'
-
-# EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# The URL or named URL pattern where requests are redirected for login when using the login_required() decorator, LoginRequiredMixin, or AccessMixin.
 LOGIN_URL = '/auth/login/'
-
-# The URL or named URL pattern where requests are redirected after logout if LogoutView doesn’t have a next_page attribute.
 LOGOUT_REDIRECT_URL = '/'
